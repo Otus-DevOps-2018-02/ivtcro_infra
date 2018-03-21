82f9274e39da2a3fa5bf32933aeea7f6cf184dd1
Victor Vavulin <vvavulin@gmail.com>
2018-03-21 23:42:36 +0300

    puma systemd unit added, deployment script chnged accordingly. gcloud command for creating VM based on reddit-full image added.

--- packer/files/prepare-vm.sh
+++ packer/files/prepare-vm.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
 cd /tmp/
-git clone -b cloud-testapp https://github.com/Otus-DevOps-2018-02/ivtcro_infra.git 
+git clone -b packer-base https://github.com/Otus-DevOps-2018-02/ivtcro_infra.git 
 cd ivtcro_infra
 
-bash install_ruby.sh
-bash install_mongodb.sh
-sudo -i -u ivtcro -S <<< "" bash /tmp/ivtcro_infra/deploy.sh
+bash ./config-scripts/install_ruby.sh
+bash ./config-scripts/install_mongodb.sh
+
+sudo -i -u ivtcro -S <<< "" bash /tmp/ivtcro_infra/packer/files/deploy.sh
 
 cd ..
 rm -fR ivtcro_infra
